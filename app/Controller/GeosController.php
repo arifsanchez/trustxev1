bfaf48efbbb2b76ab194754ce7be89bdabcbc14a
intan nabila <intannabilasalim@gmail.com>
2013-02-28 17:42:29 +0800

    test lg

--- app/Controller/GeosController.php
+++ app/Controller/GeosController.php
@@ -9,16 +9,10 @@ class GeosController extends AppController {
 
 	public function checkip(){
 		 #$ip = $_SERVER['REMOTE_ADDR'];
-		// $ip = env['HTTP_X_REAL_IP'] ||= env['REMOTE_ADDR'];
-		if(!empty($_SERVER['HTTP_CLIENT_IP'])){
-        //check ip from share internet
-        $ip = $_SERVER['HTTP_CLIENT_IP'];
-		}else if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
-        //to check ip is pass from proxy
-        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-		}else{
-        $ip = $_SERVER['REMOTE_ADDR'];
-		}
+		$ip = $_SERVER['HTTP_X_REAL_IP'];
+		debug($ip); 
+		
+		
 		 $HttpSocket = new HttpSocket();
 		/* $results = $HttpSocket->post('http://api.ipinfodb.com/v3/ip-country/',array(
 			   'key'			=>'b3305824775cffe95f11e87bad777ca407f1cb113fee069461b2bcf62cee0de5',
