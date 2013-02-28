b38ebd6a4977783353948a80988db44c4131f22e
intan nabila <intannabilasalim@gmail.com>
2013-02-28 15:57:39 +0800

    test ip json

--- app/Controller/GeosController.php
+++ app/Controller/GeosController.php
@@ -8,15 +8,20 @@ class GeosController extends AppController {
 
 
 	public function checkip(){
-		 $ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
+		 $ip = $_SERVER['REMOTE_ADDR'];
 		 $HttpSocket = new HttpSocket();
-		 $results = $HttpSocket->post('http://api.ipinfodb.com/v3/ip-country/',array(
+		/* $results = $HttpSocket->post('http://api.ipinfodb.com/v3/ip-country/',array(
 			   'key'			=>'b3305824775cffe95f11e87bad777ca407f1cb113fee069461b2bcf62cee0de5',
 			   'ip'				=>$ip,
 			   'format'		=>'json',
-			   ));  
+			   ));  */
+			   
+		$results = $HttpSocket->post('http://api.ipinfodb.com/v3/ip-country/?key=b3305824775cffe95f11e87bad777ca407f1cb113fee069461b2bcf62cee0de5&
+		ip='.$ip.'&format=json');
 		
-		debug($results);die();
+		$a = json_decode($results,true);
+		
+		debug($a);die();
 	
 	}
 
