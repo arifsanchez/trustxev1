3cc68b65f67dd24cb7d77a013960c1f8a3cc9406
intan nabila <intannabilasalim@gmail.com>
2013-02-28 15:15:12 +0800

    ip/ipinfodb

--- app/Controller/GeosController.php
+++ app/Controller/GeosController.php
@@ -1,6 +1,6 @@
 <?php
 App::uses('AppController', 'Controller');
-
+App::uses('HttpSocket', 'Network/Http');
 
 
 class GeosController extends AppController {
@@ -8,14 +8,15 @@ class GeosController extends AppController {
 
 
 	public function checkip(){
-	
-	$ip= ip2long($_SERVER['REMOTE_ADDR']);
-	$condition=array('Geo.a3 <=' =>$ip,'Geo.a4 >='=>$ip);
-	$a= $this->Geo->find('list', array(
-			'conditions' => array($condition),
-			'fields' => array('name')
-		)); 
-		debug($ip);die();
+		 $ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
+		 $HttpSocket = new HttpSocket();
+		 $results = $HttpSocket->post('http://api.ipinfodb.com/v3/ip-country/',array(
+			   'key'			=>'b3305824775cffe95f11e87bad777ca407f1cb113fee069461b2bcf62cee0de5',
+			   'ip'				=>$ip,
+			   'format'		=>'json',
+			   ));  
+		
+		debug($results);die();
 	
 	}
 
